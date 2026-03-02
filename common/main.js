56f09fefbca54d967c3909d40a7cbd4925f6826b
suedunham <42522840+suedunham@users.noreply.github.com>
2026-03-02 15:01:03 -0500

    Add current state of SMP package common files.
    I have to find a better way of tracking the changes to all the scripts in the SMP packages. A new directory has been created to hold them. Hopefully, zips can be updated and the SMP package manager will accept them.
    
    Included is the current state of play for {1583C4B7-53AD-403F-8F7E-CB20490AAA26}.zip. Changes are envisaged imminenetly.

---
+++ common/main.js
@@ -0,0 +1,12 @@
+﻿'use strict'
+
+include('Common.js');
+include('Control_List.js');
+
+function on_paint(gr) {
+    gr.DrawString(
+        'This is a utility package. It does nothing by itself.',
+        gdi.Font(g_properties.panel_message_font,
+                 g_properties.panel_message_font_size),
+        g_theme.colors.panel_text_normal, 0, 0, 0, 0);
+}
